3c4b7a3aad41d31943250871ab59320d30cb7e33
Nikita <chirkov31011993@gmail.com>
2024-10-18 18:41:48 +0500

    4.1 commit

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -13,20 +13,24 @@ const Contacts = () => {
 
                         <li className={style.content__item}>
                             <h2 className={style.title__2}>Локация</h2>
-                            <p>Россия,Уфа</p>
+                            <p className={style.content__text}>Россия,Уфа</p>
                         </li>
                         <li className={style.content__item}>
                             <h2 className={style.title__2}>
                                 Telegram / WhatsApp
                             </h2>
-                            <p>
+                            <p className={style.content__text}>
                                 <a href="tel:+79051234567">+7 (937) 347-23-27</a>
                             </p>
+                            <div className={style.wrapper__social}>
+                                <img src="/imgs/social/Telegram.jpg" alt="" />
+                                <img src="/imgs/social/WhatsApp.jpg" alt="" />
+                            </div>
                         </li>
 
                         <li className={style.content__item}>
                             <h2 className={style.title__2}>Email</h2>
-                            <p>
+                            <p className={style.content__text}>
                                 <a href="mailto:chirkov31011993@gmail.com">chirkov31011993@gmail.com</a>
                             </p>
                         </li>
